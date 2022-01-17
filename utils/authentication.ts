418878c5f0c2fe3ff44b8560be7fada9e54bd67a
googol88 <ronaldfleung@gmail.com>
2022-01-16 22:51:54 -0500

    💥 breaking: add random stuff

---
+++ utils/authentication.ts
@@ -0,0 +1,27 @@
+import { onIdTokenChanged, User } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext<{ user: User | null }>({
+  user: null,
+});
+
+export function AuthProvider({ children }: any) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    return onIdTokenChanged(AuthContext, async (user) => {
+      if (!user) {
+        setUser(null);
+        nookies.set(undefined, 'token', '', { path: '/' });
+      } else {
+        const token = await user.getIdToken();
+        setUser(user);
+        nookies.set(undefined, 'token', token, { path: '/' });
+      }
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
